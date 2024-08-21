cf0d414af89fb88e4de35b1f5382445f774de34e
Erik Bremstedt <erik.bremstedt@gmail.com>
2024-08-21 14:40:53 +0200

    fix fields

--- src/transform.py
+++ src/transform.py
@@ -15,13 +15,13 @@ def parse_json_response(data: str):
 
     parsed_json_response = []
     for hour in formatted_data['hour']:
-        ingestion_timestamp = pendulum.now().format('YYYY-MM-DD HH:mm:ss')
-        modified_timestamp = pendulum.from_format(formatted_data['location']['localtime'], 'YYYY-MM-DD HH:mm').format('YYYY-MM-DD HH:mm:ss')
+        metadata_modified = pendulum.now().format('YYYY-MM-DD HH:mm:ss')
+        data_modified = pendulum.from_format(formatted_data['location']['localtime'], 'YYYY-MM-DD HH:mm').format('YYYY-MM-DD HH:mm:ss')
         data = json.dumps(hour, indent=4)
         parsed_json_response.append(
             {
-                "ingestion_timestamp":ingestion_timestamp,
-                "modified_timestamp":modified_timestamp,
+                "metadata_modified": metadata_modified,
+                "data_modified": data_modified,
                 "data":data
             }
         )
